c4534bf9fbf9bd734107628f556c55c37ba14d4a
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-04-11 19:59:23 +0400

    feat: add solution for getPolynom task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -93,8 +93,11 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length === 1) return () => args[0];
+  if (args.length === 2) return (x) => args[0] * x + args[1];
+  if (args.length === 3) return (x) => args[0] * x ** 2 + args[1] * x + args[2];
+  return null;
 }
 
 /**
