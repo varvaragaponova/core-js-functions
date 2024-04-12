9a2e0fa53a40ccf65c28388c570e8e016e031a24
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-04-13 00:25:17 +0400

    feat: add solution for getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -236,8 +236,15 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let init = startFrom;
+  let start = startFrom;
+
+  return () => {
+    init = start;
+    start += 1;
+    return init;
+  };
 }
 
 module.exports = {
